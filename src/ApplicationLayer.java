b26ccce640f039f0f7a2a47f63d734439b93d467
yonnii <rkdtjdus753@gmail.com>
2019-10-09 16:11:37 +0900

    init project

---
+++ src/ApplicationLayer.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+
+public class ApplicationLayer implements BaseLayer{
+	public int nUpperLayerCount = 0;
+	public String pLayerName = null;
+	public BaseLayer p_UnderLayer = null;
+	public ArrayList<BaseLayer> p_aUpperLayer = new ArrayList<BaseLayer>();
+	
+	private class _CAPP_HEADER{
+		byte[] capp_totlen;
+		byte capp_type;
+		byte capp_unused;
+		byte[] capp_data;
+		
+		public _CAPP_HEADER(){
+			this.capp_totlen = new byte[2];
+			this.capp_type = 0x00;
+			this.capp_unused = 0x00;
+			this.capp_data = null;
+		}
+	}
+	
+	_CAPP_HEADER m_sHeader = new _CAPP_HEADER();
+	
+	public ApplicationLayer(String pName) {
+		//super(pName);
+		// TODO Auto-generated constructor stub
+		pLayerName = pName;
+		//ResetHeader();
+	}
+	
+	public void ResetHeader(){
+		
+	}
+	
+	public byte[] ObjToByte(_CAPP_HEADER Header, byte[] input, int length){
+	
+		return null;		
+	}
+	
+    public boolean Send(byte[] input, int length) {     	 
+    	
+		return false;
+	}
+//    
+//    public byte[] RemoveCappHeader(byte[] input, int length){
+//    
+//    }
+           
+	public boolean Receive(byte[] input){
+	
+		return true;
+	}
+
+	@Override
+	public String GetLayerName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BaseLayer GetUnderLayer() {
+		return null;
+	}
+
+	@Override
+	public BaseLayer GetUpperLayer(int nindex) {
+		return null;
+	}
+
+	@Override
+	public void SetUnderLayer(BaseLayer pUnderLayer) {
+
+	}
+
+	@Override
+	public void SetUpperLayer(BaseLayer pUpperLayer) {
+	
+	}
+
+	@Override
+	public void SetUpperUnderLayer(BaseLayer pUULayer) {
+			
+	}
+
+}
